ad27723dffdccd59ab36d5aee7c201957b32f9d3
tateyama-takayoshi <teach.heme@gmail.com>
2021-07-12 14:52:28 +0900

    試作

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true #追記
+  validates :profile, length: { maximum: 200 } #追記
+
+end
